restructured various methods; fixed mfpt transposition bug; todo make system superclass and update westpa_2 brute force landscape calculation to use new methods, them implement MSM estimation of energies and MFPTs

.ipynb_checkpoints/long_simulation-checkpoint.py
@@ -1,22 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def recover_energy_landscape(propagator, system, kT, x_init_coord, dt, nsteps, save_period, n_parallel, nbins):
+def run_long_parallel_simulations(propagator, system, kT, x_init_coord, dt, nsteps, save_period, n_parallel):
     
     #---------------------------------------------------------------
-    #determine probability distribution of n_parallel long simulations
+    #run n_parallel long simulations
     
     #reinitialize x_init each time
     x_init = np.array([x_init_coord for element in range(n_parallel)])
     long_trjs = np.array(propagator(system, kT, x_init, dt, nsteps, save_period))
-    # recorded_positions = []
 
-    # for i in range(nrounds):
-    #     x_init = propagator(x_init, prop_params[0], prop_params[1], prop_params[2], prop_params[3], prop_params[4])
-    #     #propagate(x_init, F, D, kT, dt, nsteps)
-    #     recorded_positions.append(x_init)
-        
-    #---------------------------------------------------------------
+    return long_trjs
+
+
+def estimate_energy_landscape_histogram(long_trjs, kT, nbins):
+    
     #examine the probability distribution and bin the trajectory using a histogram
     recorded_positions = long_trjs.flatten()
     
@@ -34,23 +32,9 @@ def recover_energy_landscape(propagator, system, kT, x_init_coord, dt, nsteps, s
     data_flat = np.array(recorded_positions).flatten()
     histbinned = plt.hist(data_flat, bins)
     plt.show()
-
-    z_kT = sum([np.exp(-system.potential(x)/kT) for x in bincenters])
-    eq_pops_analytic = [np.exp(-system.potential(x)/kT)/z_kT for x in bincenters]
-    plt.plot(bincenters, eq_pops_analytic)
-
+    
     eq_pops_simulation = histbinned[0]/len(data_flat)
-    plt.plot(bincenters, eq_pops_simulation)
-    
-    plt.show()
 
-    rmse_weighted = np.sqrt(np.mean([epa*(eps-epa)**2 for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)]))
-    kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)])
-    print(f"kl divergence = {kl_divergence}")
-    print(f"weighted RMSE = {rmse_weighted}")
-
-
-    
     #---------------------------------------------------------------
     #calculate and plot the energy per unit x from the probability
 
@@ -78,7 +62,7 @@ def recover_energy_landscape(propagator, system, kT, x_init_coord, dt, nsteps, s
     e_data = energies[ind_extreme:-ind_extreme]
     #plt.plot(x_data, e_data)
     
-    return x_data, e_data, long_trjs
+    return x_data, eq_pops_simulation, e_data
 
 
 #calculate mean first passage time from long trajectories

.ipynb_checkpoints/weighted_ensemble_2-checkpoint.py
@@ -288,7 +288,7 @@ def landscape_recovery(xtrj, wtrj, binbounds, transitions, hamsm_transitions, n_
     plt.show()
     
     rmse_weighted = np.sqrt(np.mean([epa*(eps-epa)**2 for epa, eps in zip(pops_norm, binned_total_weights)]))
-    kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(pops_norm, binned_total_weights)])
+    #kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(pops_norm, binned_total_weights)])
     
-    print(f"kl divergence = {kl_divergence}")
+    #print(f"kl divergence = {kl_divergence}")
     print(f"weighted RMSE = {rmse_weighted}")

energy_landscapes.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#make a 1d_system superclass and then make systems with different potential functions and macrostate classifiers subclasses that inherit its methods
 class unit_double_well():
     
     def potential(self, x):
@@ -9,7 +10,7 @@ class unit_double_well():
     def F(self, x):
         return -4*x**3 + 2*x
 
-    def plot_quantity(self, quantity):
+    def plot_quantity(self, quantity): #only useful for visualization to check that you've written the potential right; do not put in superclass
         x_extr = 1.5
         plt.plot(np.linspace(-x_extr, x_extr, 100), [quantity(i) for i in np.linspace(-x_extr, x_extr, 100)])
 
@@ -37,3 +38,85 @@ class unit_double_well():
     
     def standard_init_coord(self):
         return -1/np.sqrt(2)
+
+
+    def normalized_pops_energies(self, kT, bincenters):
+        #assume equal bin widths
+        binwidth = bincenters[1]-bincenters[0]
+
+        pops_nonnorm = [np.exp(-self.potential(x)/kT) for x in bincenters]
+        z = sum(pops_nonnorm)
+        pops_norm = [p/z for p in pops_nonnorm]
+    
+        energies_norm = [-kT*np.log(p/(z*binwidth)) for p in pops_nonnorm]
+
+        return pops_norm, energies_norm
+
+    #compute equilibrium populations of the given bins by integrating across them
+    # this should be more accurate than just using the center point of the bin
+    # the increase in accuracy provided by this method seems to be entirely unnecessary in practice
+    #tolerance is the permitted energy difference between the edges of a sub-bin in kT
+    #bin_boundaries are assumed to increase monotonically
+    def compute_true_populations(self, bin_boundaries, kT, tolerance = 0.01):
+
+        bin_centers = []
+        bin_populations = []
+
+        for i in range(len(bin_boundaries)-1):
+            bin_centers.append((bin_boundaries[i] + bin_boundaries[i+1])/2)
+
+            #figure out how many sub-bins the bin must be divided into for the potential across each bin to be roughtly constant
+            #This method assumes negligible curvature and will fail for bins with equal edge energies which curve up or down in between
+            #a more general approach would be to randomly sample points in each bin and then average or sum somehow
+            energy_gap = abs(self.potential(bin_boundaries[i+1])-self.potential(bin_boundaries[i]))/kT
+            n_subbins = max(int(np.ceil(energy_gap/tolerance)), 1)
+            
+            bin_pop = 0
+            subbin_width = (bin_boundaries[i+1] - bin_boundaries[i])/n_subbins
+            
+            for sbx in np.linspace(bin_boundaries[i]+subbin_width/2, bin_boundaries[i+1]-subbin_width/2, n_subbins):
+                bin_pop += np.exp(-self.potential(sbx)/kT)*subbin_width
+                
+            bin_populations.append(bin_pop)
+
+        z = sum(bin_populations)
+        bin_populations = [bp/z for bp in bin_populations]
+        
+        return bin_centers, bin_populations
+        
+
+#compare true and estimated energy landscapes
+def landscape_comparison(system, kT, coordinates, eq_pops_simulation, metrics = []):
+
+    #compute true populations 
+    eq_pops_analytic, energies_analytic = system.normalized_pops_energies(kT, coordinates)
+
+    #unnecessary extra precision; requires bin boundaries as an argument
+    #bincenters, eq_pops_analytic = system.compute_true_populations(bins, kT, tolerance = 0.01)
+    #plt.plot(bincenters, eq_pops_analytic)
+    
+    plt.plot(coordinates, eq_pops_analytic, linestyle="dashed")
+    plt.plot(coordinates, eq_pops_simulation)
+    
+    plt.show()
+
+    metrics = {}
+
+    if "rmsew" in metrics:
+        #without weighting, extending the region over which the RMS error is computed makes it look artificially better because there are large areas where the true and estimated probability are both about 0
+        rmse_weighted = np.sqrt(np.mean([epa*(eps-epa)**2 for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)]))
+        print(f"weighted RMSE = {rmse_weighted}")
+        metrics["rmsew"] = rmse_weighted
+
+    if "maew" in metrics:
+        #without weighting, extending the region over which the mean absolute error is computed makes it look artificially better because there are large areas where the true and estimated probability are both about 0
+        mae_weighted = np.mean([epa*abs(eps-epa) for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)])
+        print(f"weighted MAE = {mae_weighted}")            
+        metrics["maew"] = rmse_weighted
+
+    #kl divergence has bad numerical properties when any estimated entries are 0, which some usually are
+    #kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)])
+    #print(f"kl divergence = {kl_divergence}")
+
+
+    

long_simulation.py
@@ -1,84 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def recover_energy_landscape(propagator, system, kT, x_init_coord, dt, nsteps, save_period, n_parallel, nbins):
+def run_long_parallel_simulations(propagator, system, kT, x_init_coord, dt, nsteps, save_period, n_parallel):
     
     #---------------------------------------------------------------
-    #determine probability distribution of n_parallel long simulations
+    #run n_parallel long simulations
     
     #reinitialize x_init each time
     x_init = np.array([x_init_coord for element in range(n_parallel)])
     long_trjs = np.array(propagator(system, kT, x_init, dt, nsteps, save_period))
-    # recorded_positions = []
 
-    # for i in range(nrounds):
-    #     x_init = propagator(x_init, prop_params[0], prop_params[1], prop_params[2], prop_params[3], prop_params[4])
-    #     #propagate(x_init, F, D, kT, dt, nsteps)
-    #     recorded_positions.append(x_init)
-        
-    #---------------------------------------------------------------
-    #examine the probability distribution and bin the trajectory using a histogram
-    recorded_positions = long_trjs.flatten()
+    return long_trjs
+
+
+def estimate_energy_landscape_histogram(trjs, kT, nbins, binrange = [], symmetric = True):
     
-    bin_extreme = max(np.max(recorded_positions), -np.min(recorded_positions))
-    #nbins = 101 #polynomial fit is insensitive to this [verify]
+    #-------define bins--------------------------------------------------------------------
 
-    step = 2*bin_extreme/nbins
+    #flatten trajectory since the order of the frames does not matter here
+    trj_flat = trjs.flatten()
+
+    #set bin boundaries
+
+    if binrange == []:
+
+        epsilon = 10**-9 #to avoid any >= vs > issues at bin boundaries
+        if symmetric:    
+            bin_extreme = max(np.max(trj_flat), -np.min(trj_flat))
+            bin_min = -bin_extreme-epsilon
+            bin_max = bin_extreme+epsilon
+        else:
+            bin_min = np.min(trj_flat)-epsilon
+            bin_max = np.max(trj_flat)+epsilon
+    else:
+        bin_min = binrange[0]
+        bin_max = binrange[1]
+
+    step = (bin_max-bin_min)/nbins
     
-    bins = np.linspace(-bin_extreme, bin_extreme, nbins)
-    bincenters = np.linspace(-bin_extreme+step/2, bin_extreme-step/2, nbins-1)
+    binbounds = np.linspace(bin_min, bin_max, nbins+1)
+    bincenters = np.linspace(bin_min-step/2, bin_max+step/2, nbins+2)
 
-    # binned_data = np.digitize(np.array(recorded_positions).flatten(), bins)
-    # bin_counts = [np.count(binned_data, i) for i in range(nbins)]
+    #-------bin trajectory--------------------------------------------------------------------
 
-    data_flat = np.array(recorded_positions).flatten()
-    histbinned = plt.hist(data_flat, bins)
-    plt.show()
+    #note that digitize reserves the index 0 for entries below the first bin, 
+    # but in this case the bins have been constructed so that all entries lie within the explicit bin range
+    # so the first and last bins of eq_pops should be empty
+    binned_trj = np.digitize(trj_flat, bins = binbounds)
 
-    z_kT = sum([np.exp(-system.potential(x)/kT) for x in bincenters])
-    eq_pops_analytic = [np.exp(-system.potential(x)/kT)/z_kT for x in bincenters]
-    plt.plot(bincenters, eq_pops_analytic)
+    eq_pops = np.zeros(nbins+2)
+    for b in binned_trj:
+        eq_pops[b] += 1/len(trj_flat)
 
-    eq_pops_simulation = histbinned[0]/len(data_flat)
-    plt.plot(bincenters, eq_pops_simulation)
-    
-    plt.show()
-
-    rmse_weighted = np.sqrt(np.mean([epa*(eps-epa)**2 for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)]))
-    kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(eq_pops_analytic, eq_pops_simulation)])
-    print(f"kl divergence = {kl_divergence}")
-    print(f"weighted RMSE = {rmse_weighted}")
-
-
-    
-    #---------------------------------------------------------------
-    #calculate and plot the energy per unit x from the probability
+    #-------calculate energies--------------------------------------------------------------------
+    #return only the continuous central range where all bins have nonzero occupancy
 
     energies = []
-    ind_extreme = 1
-    ind_extreme_pad = 0
+    ind_min = 0
+    ind_max = nbins+1
+    x_sampled = []
+    e_sampled = []
 
     #calculate energies and identify the useful data range
-    for i, p in enumerate(histbinned[0]):
+    for i, p in enumerate(eq_pops):
         if p != 0:
             #normalized probability to calculate energy per unit x rather than per bin
-            energies.append(-kT*np.log(p/(len(np.array(recorded_positions).flatten())*step)))
+            energies.append(-kT*np.log(p/step))
+
+            x_sampled.append(bincenters[i])
+            e_sampled.append(energies[-1])
         else:
             energies.append(0)
-            if i < len(histbinned[0])/2:
-                ind_extreme = i+1
-            else:
-                ind_extreme = max(ind_extreme, len(histbinned[0])-i)
-
-    ind_extreme += ind_extreme_pad
+            if i < (nbins+2)/2:
+                ind_min = i+1
+            elif ind_max == nbins+1:
+                ind_max = i
+            
 
     #plot and fit to the sampled region
 
-    x_data = bincenters[ind_extreme:-ind_extreme]
-    e_data = energies[ind_extreme:-ind_extreme]
-    #plt.plot(x_data, e_data)
+    x_cont = bincenters[ind_min:ind_max]
+    e_cont = energies[ind_min:ind_max]
     
-    return x_data, e_data, long_trjs
+    return bincenters, eq_pops, x_sampled, e_sampled, x_cont, e_cont
 
 
 #calculate mean first passage time from long trajectories
@@ -87,7 +91,7 @@ def calc_mfpt(macrostate_classifier, n_macrostates, save_period, trajectories):
     n_transitions = np.zeros([n_macrostates, n_macrostates])
     frames_by_state = np.zeros(n_macrostates)
     
-    for trj in trajectories:
+    for trj in trajectories.transpose():
         
         #get initial state
         last_state = macrostate_classifier(trj[0])

weighted_ensemble_2.py
@@ -270,7 +270,7 @@ def landscape_recovery(xtrj, wtrj, binbounds, transitions, hamsm_transitions, n_
 
     binned_trj = np.digitize(xtrj_flat, bins = binbounds)
 
-    #+2 is for the end bins
+    #+1 is for the end bins
     binned_total_weights = np.zeros(len(binbounds)+1)
     for i, b in enumerate(binned_trj):
         binned_total_weights[b] += wtrj_flat[i]/t
@@ -288,7 +288,7 @@ def landscape_recovery(xtrj, wtrj, binbounds, transitions, hamsm_transitions, n_
     plt.show()
     
     rmse_weighted = np.sqrt(np.mean([epa*(eps-epa)**2 for epa, eps in zip(pops_norm, binned_total_weights)]))
-    kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(pops_norm, binned_total_weights)])
+    #kl_divergence = sum([epa*np.log(epa/eps) for epa, eps in zip(pops_norm, binned_total_weights)])
     
-    print(f"kl divergence = {kl_divergence}")
+    #print(f"kl divergence = {kl_divergence}")
     print(f"weighted RMSE = {rmse_weighted}")